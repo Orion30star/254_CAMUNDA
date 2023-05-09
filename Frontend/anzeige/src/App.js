dc1a0493653545e35c903c43c03efc6625b98179
Shur <89518627+JosuelCastro@users.noreply.github.com>
2023-05-09 16:00:54 +0200

    Gemacht

--- Frontend/anzeige/src/App.js
+++ Frontend/anzeige/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { startProcess } from "./Service";
@@ -72,10 +72,18 @@ function Page1({ onNextPage }) {
 function Page2({ onSubmit, onBack }) {
   const [category, setCategory] = useState("");
   const [method, setMethod] = useState("");
+  // Get the form element
+
+  const form = document.getElementById("myForm");
+
+  // Listen for form submit event
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    localStorage.setItem("category", JSON.stringify(category)); // Clear the form inputs
+    localStorage.setItem("method", JSON.stringify(method)); // Clear the form inputs
     onSubmit({ category, method });
+    window.alert("You can call this number +41 76 722 26 48");
   };
 
   return (
@@ -85,9 +93,12 @@ function Page2({ onSubmit, onBack }) {
         <label>Fall kategorisieren:</label>
         <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="">Bitte auswählen</option>
-          <option value="Option 1">Option 1</option>
-          <option value="Option 2">Option 2</option>
-          <option value="Option 3">Option 3</option>
+          <option value="Verkehrdelikt">Verkehrdelikt</option>
+          <option value="Einbruch">Einbruch</option>
+          <option value="Körperverletzung">Körperverletzung</option>
+          <option value="Sachbeschädigung">Sachbeschädigung</option>
+          <option value="Sexuelle Belästigung">Sexuelle Belästigung</option>
+
         </select>
         <br />
         <label>Kommunikationsmethode:</label>
@@ -125,37 +136,60 @@ function Page2({ onSubmit, onBack }) {
   );
 }
 
-function App() {
-  const [data, setData] = useState(null);
+function Page3({ onSubmit, onBack }) {
+  const [comment, setComment] = useState("");
 
-  const handleNextPage = (formData) => {
-    setData(formData);
-  };
+    const categoryLocalStorage = localStorage.getItem("category");
+    const methodLocalStorage = localStorage.getItem("method");
 
-  const handleBack = () => {
-    setData(null);
-  };
-
-  const handleSubmission = (formData) => {
-    setData({ ...data, ...formData });
-
-    // send updated data to Camunda as process variables
-    const variables = {
-      firstName: { value: data.firstName, type: "String" },
-      lastName: { value: data.lastName, type: "String" },
-      phone: { value: data.phone, type: "String" },
-      category: { value: data.category, type: "String" },
-      method: { value: data.method, type: "String" },
-    };
-    completeServiceTask(data.serviceTaskId, variables);
-  };
 
   return (
-    <div className="container" style={{ width: "60%", margin: "0 auto" }}>
-      {!data && <Page1 onNextPage={handleNextPage} />}
-      {data && <Page2 onSubmit={handleSubmission} onBack={handleBack} />}
+    <div>
+      <h2>Anzeigesystem</h2>
+      <b>Zusammenfassung</b>
+        <p>Kategorie: {categoryLocalStorage}</p>
+        <p>Kommunikationsmethode: {methodLocalStorage}</p>
     </div>
   );
 }
+
+  
+function App() {
+    const [data, setData] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+  
+    const handleNextPage = (formData) => {
+      setData(formData);
+      setCurrentPage(currentPage + 1);
+    };
+  
+    const handleBack = () => {
+      setData(null);
+      setCurrentPage(currentPage - 1);
+    };
+  
+    const handleSubmission = (formData) => {
+      setData({ ...data, ...formData });
+  
+      // send updated data to Camunda as process variables
+      const variables = {
+        firstName: { value: data.firstName, type: "String" },
+        lastName: { value: data.lastName, type: "String" },
+        phone: { value: data.phone, type: "String" },
+        category: { value: data.category, type: "String" },
+        method: { value: data.method, type: "String" },
+        comment: { value: formData.comment, type: "String" },
+      };
+      completeServiceTask(data.serviceTaskId, variables);
+    };
+  
+    return (
+      <div className="container" style={{ width: "60%", margin: "0 auto" }}>
+        {currentPage === 1 && <Page1 onNextPage={handleNextPage} />}
+        {currentPage === 2 && <Page2 onSubmit={handleNextPage} onBack={handleBack} />}
+        {currentPage === 3 && <Page3 onSubmit={handleSubmission} onBack={handleBack} />}
+      </div>
+    );
+  }
 
 export default App;
