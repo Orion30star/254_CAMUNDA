596a81e7332ef421f1d244f1183c37fc141ba2ba
Shur <89518627+JosuelCastro@users.noreply.github.com>
2023-05-09 12:47:52 +0200

    Commitement

--- Frontend/anzeige/src/App.js
+++ Frontend/anzeige/src/App.js
@@ -1,64 +1,143 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from "react";
+import "./App.css";
 
-const validationSchema = yup.object({
-  email: yup
-    .string('Enter your email')
-    .email('Enter a valid email')
-    .required('Email is required'),
-    anzeigeContent: yup
-    .string('Enter your anzeigeContent')
-    .min(8, 'anzeigeContent should be of minimum 8 characters length')
-    .required('anzeigeContent is required'),
-});
+function Page1({ onNextPage }) {
+  const [firstName, setFirstName] = useState("");
 
-const AnzeigeForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: 'foobar@example.com',
-      anzeigeContent: 'Was ist Ihre Anzeige?',
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  const [lastName, setLastName] = useState("");
+
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onNextPage({ firstName, lastName, phone });
+  };
 
   return (
     <div>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
+            <h2>Page 1</h2>
+           {" "}
+      <form onSubmit={handleSubmit}>
+                <label>Vorname:</label>
+               {" "}
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
         />
-        <TextField
-          fullWidth
-          id="anzeigeContent"
-          name="anzeigeContent"
-          label="anzeigeContent"
-          type="anzeigeContent"
-          value={formik.values.anzeigeContent}
-          onChange={formik.handleChange}
-          error={formik.touched.anzeigeContent && Boolean(formik.errors.anzeigeContent)}
-          helperText={formik.touched.anzeigeContent && formik.errors.anzeigeContent}
+                <br />
+                <label>Nachname:</label>
+               {" "}
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
         />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
+                <br />
+                <label>Telefonnummer:</label>
+               {" "}
+        <input
+          type="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+                <br />
+                <button type="submit">Weiter</button>
+             {" "}
       </form>
+         {" "}
     </div>
   );
-};
+}
 
-ReactDOM.render(<AnzeigeForm />, document.getElementById('root'));
-export default AnzeigeForm;
+function Page2({ onSubmit }) {
+  const [category, setCategory] = useState("");
+
+  const [method, setMethod] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onSubmit({ category, method });
+  };
+
+  return (
+    <div>
+            <h2>Page 2</h2>
+           {" "}
+      <form onSubmit={handleSubmit}>
+                <label>Fall kategorisieren:</label>
+               {" "}
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="">Bitte auswählen</option>
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 3">Option 3</option>
+                 {" "}
+        </select>
+                <br />
+                <label>Kommunikationsmethode:</label>
+               {" "}
+        <div>
+                   {" "}
+          <label>
+                       {" "}
+            <input
+              type="radio"
+              name="method"
+              value="Telefonat"
+              checked={method === "Telefonat"}
+              onChange={(e) => setMethod(e.target.value)}
+            />
+                        Telefonat          {" "}
+          </label>
+                 {" "}
+        </div>
+               {" "}
+        <div>
+                   {" "}
+          <label>
+                       {" "}
+            <input
+              type="radio"
+              name="method"
+              value="Textfeld"
+              checked={method === "Textfeld"}
+              onChange={(e) => setMethod(e.target.value)}
+            />
+                        Textfeld          {" "}
+          </label>
+                 {" "}
+        </div>
+                <br />
+                <button type="submit">Abschicken</button>
+             {" "}
+      </form>
+         {" "}
+    </div>
+  );
+}
+
+function App() {
+  const [data, setData] = useState(null);
+
+  const handleNextPage = (formData) => {
+    setData(formData);
+  };
+
+  const handleSubmission = (formData) => {
+    setData({ ...data, ...formData });
+
+    console.log("Submitted data:", { ...data, ...formData });
+  };
+
+  return (
+    <div>
+            {!data && <Page1 onNextPage={handleNextPage} />}
+            {data && <Page2 onSubmit={handleSubmission} />}
+         {" "}
+    </div>
+  );
+}
+export default App;
--- Frontend/anzeige/src/index.js
+++ Frontend/anzeige/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AnzeigeForm from './App';
-import Entry from './entry';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Entry/>
+    <App/>
   </React.StrictMode>
 );
 
